38ca631f577a954938ae62cb603dc8c5e34624ca
AkiWeb2 <153662065+AkiWeb2@users.noreply.github.com>
2023-12-15 23:38:11 +0400

    Refraction of rays

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "фактический показатель преломления среды:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(262, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ответ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(401, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(346, 252);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пример графика:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Сохранить ответ в PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 106);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Вода";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(15, 83);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Стекло";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(15, 129);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(59, 17);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "Алмаз";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 473);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Определение показателя преломления среды.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,274 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox pictureBox;
+        private Label label;
+        private TextBox textBox;
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        private iTextSharp.text.Font helvetica;
+        private BaseFont helveticaBase;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            ClientSize = new Size(800, 310);
+
+            pictureBox = new PictureBox
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size(250, 200),
+                Location = new Point(100, 25),
+            };
+            label = new Label
+            {
+                Text = "Введите угол падения (в градусах):",
+                Size = new Size(200, 20),
+                Location = new Point(100, 230),
+            };
+
+            textBox = new TextBox
+            {
+                Size = new Size(50, 20),
+                Location = new Point(110, 250),
+            };
+            Controls.Add(pictureBox);
+            Controls.Add(label);
+            Controls.Add(textBox);
+            textBox.TextChanged += UpdatePictureBox;
+        }
+
+        private void UpdatePictureBox(object sender, EventArgs e)
+        {
+            pictureBox.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            float angle;
+            bool parseSuccess = float.TryParse(textBox.Text, out angle);
+            if (angle > 0)
+            {
+                if (parseSuccess)
+                {
+                    // Вычислить показатель преломления среды и угол преломления
+                    float refractiveIndex = 1.5f; // Здесь необходимо указать фактический показатель преломления среды
+                    float WaterIndex = 1.33f;
+                    float AlmIndex = 2.42f;
+                    float angleOfReWater = (float)(Math.Asin(Math.Sin(angle * Math.PI / 180) / WaterIndex) * 180 / Math.PI);
+                    float angleOfReAlm = (float)(Math.Asin(Math.Sin(angle * Math.PI / 180) / AlmIndex) * 180 / Math.PI);
+                    float angleOfRefraction = (float)(Math.Asin(Math.Sin(angle * Math.PI / 180) / refractiveIndex) * 180 / Math.PI);
+                    label2.Text = angleOfRefraction.ToString();
+
+                    // Очистить PictureBox и нарисовать лучи
+                    using (var graphics = pictureBox.CreateGraphics())
+                    {
+                        graphics.Clear(Color.White);
+                        Pen normalPen = new Pen(Color.Black, 2);
+                        Pen normalPenP = new Pen(Color.Black, 2);
+                        normalPenP.DashStyle = DashStyle.Dash;
+                        graphics.DrawLine(normalPenP, new Point(0, 50), new Point(275, 50));
+                        graphics.DrawLine(normalPen, new Point(133, 0), new Point(133, 200));
+
+                        Pen incidentRayPen = new Pen(Color.Red, 2);
+                        Pen incidentRay = new Pen(Color.Orange, 2);
+                        if (angle > 180)
+                        {
+                            incidentRay.DashStyle = DashStyle.Dash;
+                            graphics.DrawLine(incidentRayPen, new Point(20, (int)angle + 40), new Point(130, 50));
+                            graphics.DrawLine(incidentRay, new Point(20, (int)angle - 40), new Point(130, 50));
+                        }
+                        else if (angle > 90 && angle != 180)
+                        {
+                            incidentRay.DashStyle = DashStyle.Dash;
+                            graphics.DrawLine(incidentRayPen, new Point(130, 50), new Point(285, (int)angle + 60));
+                            graphics.DrawLine(incidentRay, new Point(130, 50), new Point(285, (int)angle - 60));
+                        }
+                        else if (angle <= 90)
+                        {
+                            incidentRay.DashStyle = DashStyle.Dash;
+                            graphics.DrawLine(incidentRayPen, new Point(130, 50), new Point(285, (int)angle - 40));
+                            graphics.DrawLine(incidentRay, new Point(130, 50), new Point(285, (int)angle + 40));
+                        }
+                        else if (angle == 180)
+                        {
+                            incidentRay.DashStyle = DashStyle.Dash;
+                            graphics.DrawLine(incidentRayPen, new Point(133, 50), new Point(133, 200));
+                            graphics.DrawLine(incidentRay, new Point(133, 50), new Point(285, -(int)angle + 40));
+                        }
+
+
+                        Pen refractedRayPen = new Pen(Color.Blue, 2);
+                        if (checkBox2.Checked == true)
+                        {
+                            double Px = angleOfRefraction + 100 * Math.Cos(angle);
+                            //double Py = angleOfRefraction + 100 * Math.Sin(angle);
+                            float arrowAngle = (float)(Math.Atan2(25, 50 - 125) * 180 / Math.PI);
+                            if (angle < 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(135, 50), new Point((int)-arrowAngle, (int)-Px - 40));
+                            }
+                            else if (angle > 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(133, 50), new Point((int)arrowAngle, (int)Px + 40));
+                            }
+                            else if (angle == 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(135, 50), new Point((int)-arrowAngle, (int)Px));
+                            }
+                        }
+                        else if (checkBox1.Checked == true)
+                        {
+                            double Px = angleOfReWater + 100 * Math.Cos(angle);
+                            //double Py = angleOfRefraction + 100 * Math.Sin(angle);
+                            float arrowAngle = (float)(Math.Atan2(25, 50 - 125) * 180 / Math.PI);
+                            if (angle < 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(135, 50), new Point((int)-arrowAngle, (int)-Px - 40));
+                            }
+                            else if (angle > 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(133, 50), new Point((int)arrowAngle, (int)Px + 40));
+                            }
+                            else if (angle == 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(135, 50), new Point((int)-arrowAngle, (int)Px));
+                            }
+                        }
+                        else if (checkBox3.Checked == true)
+                        {
+                            double Px = AlmIndex + 100 * Math.Cos(angle);
+                            //double Py = angleOfRefraction + 100 * Math.Sin(angle);
+                            float arrowAngle = (float)(Math.Atan2(25, 50 - 125) * 180 / Math.PI);
+                            if (angle < 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(135, 50), new Point((int)-arrowAngle, (int)-Px - 40));
+                            }
+                            else if (angle > 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(133, 50), new Point((int)arrowAngle, (int)Px + 40));
+                            }
+                            else if (angle == 180)
+                            {
+                                graphics.DrawLine(refractedRayPen, new Point(135, 50), new Point((int)-arrowAngle, (int)Px));
+                            }
+                        }
+                        graphics.DrawLine(refractedRayPen, new Point(275, 100), new Point(275 - 10, 100 - 5));
+                        graphics.DrawLine(refractedRayPen, new Point(275, 100), new Point(275 - 10, 100 + 5));
+                        if (angle <= 180)
+                        {
+                            graphics.DrawString("θ1", Font, Brushes.Red, new PointF(150, 35));
+                            graphics.DrawString("θ2", Font, Brushes.Blue, new PointF(55, 60));
+                        }
+                        else if (angle > 180)
+                        {
+                            graphics.DrawString("θ1", Font, Brushes.Red, new PointF(55, 60));
+                            graphics.DrawString("θ2", Font, Brushes.Blue, new PointF(150, 35));
+                        }
+
+
+                        float angleOfIncidenceLabelX = (225 + 125) / 2 - 10;
+                        float angleOfRefractionLabelX = (25 + 75) / 2 - 10;
+                        if (checkBox2.Checked == true)
+                        {
+                            if (angle <= 180)
+                            {
+                                graphics.DrawString(angle.ToString(), Font, Brushes.Black, new PointF(angleOfIncidenceLabelX, 55));
+                                graphics.DrawString(angleOfRefraction.ToString(), Font, Brushes.Black, new PointF(angleOfRefractionLabelX, 35));
+                            }
+                            else if (angle > 180)
+                            {
+                                graphics.DrawString(angle.ToString(), Font, Brushes.Black, new PointF(angleOfRefractionLabelX, 50));
+                                graphics.DrawString(angleOfRefraction.ToString(), Font, Brushes.Black, new PointF(angleOfIncidenceLabelX, 40));
+                            }
+                        }
+                        else if (checkBox1.Checked == true)
+                        {
+                            if (angle <= 180)
+                            {
+                                graphics.DrawString(angle.ToString(), Font, Brushes.Black, new PointF(angleOfIncidenceLabelX, 55));
+                                graphics.DrawString(angleOfReWater.ToString(), Font, Brushes.Black, new PointF(angleOfRefractionLabelX, 35));
+                            }
+                            else if (angle > 180)
+                            {
+                                graphics.DrawString(angle.ToString(), Font, Brushes.Black, new PointF(angleOfRefractionLabelX, 50));
+                                graphics.DrawString(angleOfReWater.ToString(), Font, Brushes.Black, new PointF(angleOfIncidenceLabelX, 40));
+                            }
+                        }
+                        else if (checkBox3.Checked == true)
+                        {
+                            if (angle <= 180)
+                            {
+                                graphics.DrawString(angle.ToString(), Font, Brushes.Black, new PointF(angleOfIncidenceLabelX, 55));
+                                graphics.DrawString(angleOfReAlm.ToString(), Font, Brushes.Black, new PointF(angleOfRefractionLabelX, 35));
+                            }
+                            else if (angle > 180)
+                            {
+                                graphics.DrawString(angle.ToString(), Font, Brushes.Black, new PointF(angleOfRefractionLabelX, 50));
+                                graphics.DrawString(angleOfReAlm.ToString(), Font, Brushes.Black, new PointF(angleOfIncidenceLabelX, 40));
+                            }
+                        }
+                    }
+                }
+            }else if (angle < 0)
+            {
+                while (angle < 0)
+                {
+                    MessageBox.Show("Угол не может быть меньше 0");
+                    break;
+                }  
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.FormBorderStyle = FormBorderStyle.None;
+            //this.AllowTransparency = true;
+            this.BackColor = Color.AliceBlue;//цвет фона  
+            //this.TransparencyKey = this.BackColor;//он же будет заменен на прозрачный цвет
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var document = new iTextSharp.text.Document();
+            using (var writer = PdfWriter.GetInstance(document, new FileStream("result.pdf", FileMode.Create)))
+            {
+                document.Open();
+
+                helvetica = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12);
+                helveticaBase = helvetica.GetCalculatedBaseFont(false);
+                PdfPTable table = new PdfPTable(3);
+                PdfPCell cell = new PdfPCell(new Phrase(" ",
+                 new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 16,
+                 iTextSharp.text.Font.NORMAL, new BaseColor(Color.Black))));
+                cell.BackgroundColor = new BaseColor(Color.White);
+                cell.Padding = 1;
+                cell.Colspan = 1;
+                cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                table.AddCell(cell);
+                table.AddCell(textBox.Text);
+                table.AddCell(label2.Text);
+                table.AddCell(cell);
+                document.Add(table);
+                document.Close();
+                writer.Close();
+            }
+        }
+    }
+}
